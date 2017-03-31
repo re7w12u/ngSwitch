29298afb6cddc3ec19f3dd3fc792b4e80ea49445
Julien <julien.bessiere@non.schneider-electric.com>
2017-03-31 09:00:55 +0200

    adding label

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MissionListComponent } from './mission-list/mission-list.component';
+import { SwitchComponent } from './switch/switch.component';
+import { MissionService } from './mission.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MissionListComponent,
+    SwitchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [MissionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
